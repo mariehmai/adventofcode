bbead84e827be189d7c1c5b3530da4d139349a1e
mariehmai <mai.mariehelene@gmail.com>
2021-01-02 03:23:44 +0100

    feat(puzzle1): added solution part2

--- puzzle1.kt
+++ puzzle1.kt
@@ -3,6 +3,7 @@ import java.io.File
 fun main() {
   val numbers = readFileAsLinesUsingBufferedReader("./input1.txt").map(String::toInt)
   println("[Part1] product result: ${part1(numbers)}")
+  println("[Part2] product result: ${part2(numbers)}")
 }
 
 fun readFileAsLinesUsingBufferedReader(fileName: String): List<String>
@@ -27,4 +28,35 @@ fun findPairMatchingSum(numbers: List<Int>, sum: Int): Pair<Int, Int> {
   }
 
   return Pair(0, 0)
+}
+
+
+fun part2(numbers: List<Int>): Int {
+  val inputs = findTripleMatchingSum(numbers.sorted(), 2020)
+  println("[Part2] inputs to multiply: ${inputs}")
+  
+  return product(inputs.toList())
+}
+
+fun findTripleMatchingSum(numbers: List<Int>, sum: Int): Triple<Int, Int, Int> {
+  for (i in 0 until numbers.count() - 3 step 1) {
+    var k = sum - numbers[i];
+    var low = i + 1;
+    var high = numbers.count() - 1;
+
+    while (low < high)
+    {
+      if (numbers[low] + numbers[high] < k) {
+        low++;
+      }
+      else if (numbers[low] + numbers[high] > k) {
+        high--;
+      }
+      else {
+        return Triple(numbers[i], numbers[low], numbers[high])
+      }
+    }
+  }
+  
+  return Triple(0, 0, 0)
 }
